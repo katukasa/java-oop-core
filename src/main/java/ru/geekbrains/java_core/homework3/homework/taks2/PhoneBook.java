770d46ed01ce2c876a255d3f89788a67bb801f63
zwr6sn <ekaterina.sheldukova@siemens.com>
2020-09-27 20:15:40 +0200

    lesson 4 update for PhoneBook

--- src/main/java/ru/geekbrains/java_core/homework3/homework/taks2/PhoneBook.java
+++ src/main/java/ru/geekbrains/java_core/homework3/homework/taks2/PhoneBook.java
@@ -4,19 +4,19 @@ import java.util.*;
 
 public class PhoneBook {
 
-    private Map<String, Set<String>> listOfNumbers = new HashMap<>();
+    private Map<String, HashSet<Object>> listOfNumbers = new HashMap<>();
 
     public void add(String name, String phone){
         if(!listOfNumbers.containsKey(name)){
             listOfNumbers.put(name, new HashSet<>());
         }
-        Set<String> values = listOfNumbers.get(name);
+        Set<Object> values = listOfNumbers.get(name);
         values.add(phone);
     }
 
 //    public Set<String> get(String name){
     public void get(String name){
-        Set<String> values = listOfNumbers.get(name);
+        Set<Object> values = listOfNumbers.get(name);
         System.out.println(name + ": " + values);
 //        return values;
     }
